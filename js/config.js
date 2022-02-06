41e42be4ec6c8727b551fa954bf618dbc71ff2f6
Atomic Jelly <33538495+HimLee@users.noreply.github.com>
2022-02-06 14:11:20 +1100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,18 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_2Dg1dEvHJ',
+        region: 'us-east-1',
+        clientId: '3hft9ha7r56ui6rhgff89v5uk4'
+    },
+
+    api: {
+        apiGetAllUrl: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-get-all-info", 
+        // GET
+        deleteTagUrl: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-delete-tags", 
+        deleteImageUrl: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-delete-image?s3-url=", 
+        uploadUrl: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-upload",
+        findByTagUrl: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-find-3-2-1",
+        fingByImageUrl: " https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/api-find-3-2-2",
+        delete3Url: "https://2td2npradb.execute-api.us-east-1.amazonaws.com/Testing/delete-3"
+    }
+}
